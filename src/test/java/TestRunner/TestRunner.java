101f298dbcb27a90d9de36681ed60e68eea3db8c
Sanjay Singh Mehara <mehracious619@gmail.com>
2025-09-07 18:52:57 +0530

    fourth commit

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
  
 @CucumberOptions(
-	    features = "src/test/resources/Features/SearchFilterPage.feature",
+	    features = "src/test/resources/Features/SearchFilter.feature",
 	    glue = {"StepDefinitions","Hooks"},
 //	    tags="@sanity",
 	    plugin = {
